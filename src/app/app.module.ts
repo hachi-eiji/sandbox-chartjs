56463e696e27cd9c3dff0fb93e87e5de3f042b66
Eiji Hachiya <eiji.hachiya@classi.jp>
2019-08-26 16:26:26 +0900

    LineChartDirectiveを追加してみた

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './components/bar-chart/bar-chart.component';
-import { BarChartDirective } from './directives/bar-chart.directive';
 import { MainComponent } from './components/main/main.component';
 import { TimelineComponent } from './components/timeline/timeline.component';
+import { BarChartDirective } from './directives/bar-chart.directive';
+import { LineChartDirective } from './directives/line-chart.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarChartDirective,
     BarChartComponent,
+    LineChartDirective,
     MainComponent,
     TimelineComponent
   ],
